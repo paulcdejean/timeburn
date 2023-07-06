93c1afebd35b85c8a8e13fc2777475cf35afe050
Paul Dejean <paulcdejean@gmail.com>
2023-07-06 18:19:55 -0500

    Detect when component is unmounted...

--- src/mains/mainBasic.ts
+++ src/mains/mainBasic.ts
@@ -6,6 +6,12 @@ export const basicFunctions = Object.keys(basicList)
 
 export async function mainBasic(ns: NS): Promise<void> {
   
+  await new Promise(resolve => {
+    ns.tprintRaw(React.createElement(Test, {resolveCallback: resolve}))
+  })
 
-  ns.tprintRaw(React.createElement(Test))
+  ns.tprint("Component is unmounted")
+  
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
 }
--- src/quests/ui/Test.tsx
+++ src/quests/ui/Test.tsx
@@ -1,7 +1,17 @@
 import cssInline from "./css/Test.module.css?inline"
 import css from "./css/Test.module.css"
 
-function Test() {
+export interface TestProps {
+  resolveCallback: Function
+}
+
+function Test(props: TestProps) {
+  React.useEffect(() => {
+    return () => {
+      props.resolveCallback()
+    }
+}, [])
+
   return (
     <>
       <style>{cssInline}</style>
