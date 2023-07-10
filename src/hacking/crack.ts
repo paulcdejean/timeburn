30d20f64de20b10badf5a8cae1cd171abc35ad34
Paul Dejean <paulcdejean@gmail.com>
2023-07-09 23:10:08 -0500

    Pull out getResetInfo.

--- src/hacking/crack.ts
+++ src/hacking/crack.ts
@@ -78,36 +78,29 @@ function everythingIsCracked(network: Network): boolean {
 }
 
 function updateCracks(ns: NS, network: Network) : boolean {
-  const timestamp = ns.tFormat(Date.now() - ns.getResetInfo().lastAugReset)
   let cracksUpdated = false
 
   if (!network.cracks[Cracks.NUKE] && ns.fileExists(Cracks.NUKE)) {
-    ns.tprint(`${Cracks.NUKE} aquired at ${timestamp}`)
     network.cracks[Cracks.NUKE] = true
     cracksUpdated = true
   }
   if (!network.cracks[Cracks.BruteSSH] && ns.fileExists(Cracks.BruteSSH)) {
-    ns.tprint(`${Cracks.BruteSSH} aquired at ${timestamp}`)
     network.cracks[Cracks.BruteSSH] = true
     cracksUpdated = true
   }
   if (!network.cracks[Cracks.FTPCrack] && ns.fileExists(Cracks.FTPCrack)) {
-    ns.tprint(`${Cracks.FTPCrack} aquired at ${timestamp}`)
     network.cracks[Cracks.FTPCrack] = true
     cracksUpdated = true
   }
   if (!network.cracks[Cracks.RelaySMTP] && ns.fileExists(Cracks.RelaySMTP)) {
-    ns.tprint(`${Cracks.RelaySMTP} aquired at ${timestamp}`)
     network.cracks[Cracks.RelaySMTP] = true
     cracksUpdated = true
   }
   if (!network.cracks[Cracks.HTTPWorm] && ns.fileExists(Cracks.HTTPWorm)) {
-    ns.tprint(`${Cracks.HTTPWorm} aquired at ${timestamp}`)
     network.cracks[Cracks.HTTPWorm] = true
     cracksUpdated = true
   }
   if (!network.cracks[Cracks.SQLInject] && ns.fileExists(Cracks.SQLInject)) {
-    ns.tprint(`${Cracks.SQLInject} aquired at ${timestamp}`)
     network.cracks[Cracks.SQLInject] = true
     cracksUpdated = true
   }
